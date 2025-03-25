0fccee70762e1ba31a2c4b6abaac4e5756d5012d
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-03-26 00:49:56 +0100

    - Update README - integrate Tailwind CSS

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage: React.FC = () => {
     return (
         <div style={{ textAlign: 'center', padding: '20px' }}>
-            <h1>Welcome to My Portfolio</h1>
+    <h1 className="text-3xl font-bold underline">Welcome to My Portfolio</h1>
             <p>This is the home page of my portfolio website.</p>
         </div>
     );
